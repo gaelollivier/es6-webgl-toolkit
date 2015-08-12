31df178a61c98e65a710bbfef1d2147a7234f189
Gael du Plessix <gael.duplessix@gmail.com>
2015-08-12 22:29:25 +0200

    Add Geometry and Renderer classes
    Split Program so it only deals with the program. Add a geometry class
    to handle vertices attributes and a renderer to… render

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,7 +1,8 @@
 precision mediump float;
 
 varying vec2 position;
+varying vec3 color;
 
 void main() {
-	gl_FragColor = vec4(position.x, 0.0, 0.0, 1.0);
+	gl_FragColor = vec4(color, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,10 +1,13 @@
 precision mediump float;
 
-attribute vec3 vertexPosition;
+attribute vec2 vertexPosition;
+attribute vec3 vertexColor;
 
 varying vec2 position;
+varying vec3 color;
 
 void main() {
 	position = vec2(vertexPosition.xy) * 0.5 + 0.5;
-	gl_Position = vec4(vertexPosition, 1.0);
+    color = vertexColor;
+	gl_Position = vec4(vertexPosition, 0.0, 1.0);
 }
