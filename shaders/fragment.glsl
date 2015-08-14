629c3b615fc52e468e022e845ff3052346059ec8
Gael du Plessix <gael.duplessix@gmail.com>
2015-08-14 21:11:18 +0200

    Add support for uniforms and clean code
    Add comments and replace console.error with exceptions

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,14 @@
 precision mediump float;
 
 varying vec2 position;
-varying vec3 color;
+
+uniform float time;
 
 void main() {
+    vec3 color = vec3(1.0);
+
+    float distToCenter = length(position - vec2(0.5, 0.5));
+    color *= sin(distToCenter * 20.0 - time * 10.0);
+
 	gl_FragColor = vec4(color, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,13 +1,10 @@
 precision mediump float;
 
 attribute vec2 vertexPosition;
-attribute vec3 vertexColor;
 
 varying vec2 position;
-varying vec3 color;
 
 void main() {
 	position = vec2(vertexPosition.xy) * 0.5 + 0.5;
-    color = vertexColor;
 	gl_Position = vec4(vertexPosition, 0.0, 1.0);
 }
